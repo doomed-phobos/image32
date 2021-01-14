d42766dae964374163c518c0648978a9265328d4
TheEKJ <the_ekj.2121@outlook.com>
2021-01-14 18:34:05 -0500

    how long does the decoding take?

---
+++ src/time.h
@@ -0,0 +1,20 @@
+#ifndef TIME_H
+#define TIME_H
+#include <cstdio>
+#include <ctime>
+
+class Timer
+{
+public:
+   Timer() {m_start = clock();}
+   ~Timer() {
+      m_end = clock();
+      double secs = (double)(m_end - m_start) / CLOCKS_PER_SEC;
+
+      printf("Tiempo: %f\n", secs);
+   }
+private:
+   std::clock_t m_start, m_end;
+};
+
+#endif
