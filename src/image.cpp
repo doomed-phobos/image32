e449e8e0e2816a307e3f639105c3575ddc00aec2
TheEKJ <the_ekj.2121@outlook.com>
2021-02-07 10:12:34 -0500

    Update Image

--- src/image.cpp
+++ src/image.cpp
@@ -6,18 +6,36 @@
 
 namespace img32
 {
+   Image::Image()
+   {}
+
    Image::Image(const ImageInfo& info) :
    m_info(info)
    {
-      m_requiredSize = m_info.getRowStrideBytes()*height();
+      m_totalSize = m_info.getRowStrideBytes()*height();
 
-      m_pixels.resize(m_requiredSize);
+      m_buffer.resize(m_totalSize);
+   }
+
+   const void* Image::getPixels() const
+   {
+      return m_buffer.buffer();
+   }
+
+   void* Image::getPixels()
+   {
+      return m_buffer.buffer();
+   }
+
+   void Image::setPixels(uint8_t* pixels)
+   {
+      std::copy(pixels, pixels + m_totalSize, m_buffer.begin());
    }
 
    const_address_t Image::addr32() const
    {
       assert(m_info.getBytesPerPixel() == 4);
-      return reinterpret_cast<const_address_t>(&m_pixels[0]);
+      return reinterpret_cast<const_address_t>(m_buffer.buffer());
    }
 
    const_address_t Image::addr32(int x, int y) const
@@ -32,6 +50,25 @@ namespace img32
       return const_cast<address_t>(addr32(x, y));
    }
 
+   void Image::fill(color_t c)
+   {
+      address_t first = writable_addr32(0, 0);
+      std::fill(first, first+width(), c);
+
+      for(int y = 1; y < height(); y++)
+         std::copy(first, first+width(), writable_addr32(0, y));
+   }
+
+   void Image::putPixel(int x, int y, color_t c)
+   {
+      *writable_addr32(x, y) = c;
+   }
+
+   color_t Image::getPixel(int x, int y) const
+   {
+      return *addr32(x, y);
+   }
+
    bool Image::isValid() const
    {
       return width() > 0 && height() > 0;
@@ -41,6 +78,12 @@ namespace img32
    {
       ImageIO io(filename);
       return io.decode(this, ct);
+   }
+
+   bool Image::saveToFilename(const_charp filename, const EncoderOptions& eo)
+   {
+      ImageIO io(filename);
+      return io.encode(*this, eo);
    }
 
    void Image::reset(const ImageInfo& info)
