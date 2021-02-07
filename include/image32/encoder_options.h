a4dd2b92faa7506b9b7524af4c49e9af5d2df7c5
TheEKJ <the_ekj.2121@outlook.com>
2021-02-07 10:08:31 -0500

    Add alpha bitmask

--- include/image32/encoder_options.h
+++ include/image32/encoder_options.h
@@ -4,22 +4,44 @@
 
 namespace img32
 {
-   struct EncoderOptions
+   struct IMG32_DLL EncoderOptions
    {
       enum ColorType {
-         RGBA_ColorType,
-         GRAYSCALE_ColorType,
-         INDEXED_ColorType
+         Alpha_BitMask        = 0x08,
+         RGB_ColorType        = 0x02,
+         RGBA_ColorType       = 0x02 | Alpha_BitMask,
+         Grayscale_ColorType  = 0x04,
+         GrayscaleA_ColorType = 0x04 | Alpha_BitMask
       };
 
+      ColorType colortype;
+      /// Si es true, se arregla la exportacion de grayscale
+      /// para imagenes pequeñas.
+      /// De lo contrario, la exportacion no sería perfecta
+      bool fix_grayscale;
 
       //========================
       // JPG
       //========================
-      ColorType colortype;
       int jpg_quality;
-      // Color que reemplazará a los colores transparentes
       color_t jpg_background;
+
+      color_t bmp_background;
+
+      /// Retorna una estructura EncoderOptions con valores por defecto:
+      /// colortype = RGBA
+      /// fix_grayscale = false
+      /// jpg_quality = 100
+      /// jpg_background = White
+      static EncoderOptions Default() {
+         EncoderOptions opt;
+         opt.colortype = RGB_ColorType;
+         opt.jpg_quality = 100;
+         opt.jpg_background = rgba(255, 255, 255);
+         opt.fix_grayscale = false;
+         opt.bmp_background = rgba(255, 255, 255);
+         return opt;
+      }
    };
 } // namespace img32
 
