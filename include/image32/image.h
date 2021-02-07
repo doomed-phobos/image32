7add25a74d80325e0a2b5034f3a032f2dfe65531
TheEKJ <the_ekj.2121@outlook.com>
2021-02-07 10:09:16 -0500

    Create ImageBuffer

--- include/image32/image.h
+++ include/image32/image.h
@@ -1,18 +1,18 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 #include "image_info.h"
-
-#include <vector>
+#include "image_buffer.h"
+#include "encoder_options.h"
 
 namespace img32
 {
    /// Clase principal de esta librería que contiene lo que compone una imagen
    /// Para cargar desde un archivo use Image::loadFromFilename
-   class IMG32_API Image
+   class IMG32_DLL Image
    {
    public:
       /// Crear una imagen vacia (Width = 0 y Height = 0)
-      Image() {}
+      Image();
       /// Crea una clase Image con formato de pixel 'RGB' por defecto
       Image(const ImageInfo& info);
 
@@ -23,11 +23,17 @@ namespace img32
       /// Asegurese que el arreglo de pixeles coincida con el total
       /// de pixeles (RowStrideBytes * ImageHeight)
       //void setPixels(address_t pixels);
-      void* getPixels() {return &m_pixels[0];}
+      const void* getPixels() const;
+      void* getPixels();
+      void setPixels(uint8_t* pixels);
 
       const_address_t addr32() const;
       const_address_t addr32(int x, int y) const;
       address_t writable_addr32(int x, int y) const;
+
+      void fill(color_t c);
+      void putPixel(int x, int y, color_t c);
+      color_t getPixel(int x, int y) const;
 
       size_t rowBytes() const {return m_info.minRowBytes();}
       int width() const {return m_info.width();}
@@ -39,16 +45,18 @@ namespace img32
 
       /// Carga una imagen con un tipo determinado de color dentro
       /// de un ImgIO sin ninguna configuracion
-      bool loadFromFilename(const char filename[], ColorType ct);
-      bool loadFromFilename(const char filename[]) {
+      bool loadFromFilename(const_charp filename, ColorType ct);
+      bool loadFromFilename(const_charp filename) {
          return loadFromFilename(filename, colorType());
       }
 
+      bool saveToFilename(const_charp filename, const EncoderOptions& eo);
+
       void reset(const ImageInfo& info);
    private:
-      size_t m_requiredSize;
+      size_t m_totalSize;
       ImageInfo m_info;
-      std::vector<color_t> m_pixels;
+      ImageBuffer m_buffer;
    };
 } // namespace img32
 
