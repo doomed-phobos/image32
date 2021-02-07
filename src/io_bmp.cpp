790c95b863f88bd6137ada6e3f925fe2e2262266
TheEKJ <the_ekj.2121@outlook.com>
2021-02-07 10:12:19 -0500

    Update io_bmp

--- src/io_bmp.cpp
+++ src/io_bmp.cpp
@@ -5,8 +5,13 @@
 
 #include <cstring>
 #include <csetjmp>
-
+//TODO: Crear el codificador de imagenes BMP
 #define BMP_FIELD 0x4D42
+
+#define BMP_INFO_HEADER_SIZE 40
+#define BMP_HEADER_SIZE 54
+#define BMP_DEFAULT_PLANES 1
+
 #define BMP_ERROR(bmp, msg) bmp->error_fn(bmp, msg)
 
 namespace img32::priv
@@ -23,6 +28,8 @@ namespace img32::priv
       uint16_t bitCount;
       uint16_t planes;
       uint16_t compression;
+      EncoderOptions::ColorType color_type;
+      uint32_t filler;
    }*bmp_header_ptr;
 
    typedef struct bmp_struct
@@ -35,7 +42,22 @@ namespace img32::priv
       bmp_voidp data; //Reserved. NOT USED
    }*bmp_struct_ptr;
 
-   void bmp_start_stdio(bmp_struct_ptr bmp, FILE* file)
+   uint32_t bmp_get_width(bmp_struct_ptr bmp) {return bmp->header.width;}
+   uint32_t bmp_get_height(bmp_struct_ptr bmp) {return bmp->header.height;}
+   EncoderOptions::ColorType bmp_get_color_type(bmp_struct_ptr bmp) {return bmp->header.color_type;}
+   uint16_t bmp_get_bitcount(bmp_struct_ptr bmp) {return bmp->header.bitCount;}
+   void bmp_set_info(bmp_struct_ptr bmp, int width, int height, EncoderOptions::ColorType color_type)
+   {
+      bmp->header.width = width;
+      bmp->header.height = height;
+      bmp->header.color_type = color_type;
+   }
+
+///////////////////////////////////////////////////////////////////////////////////////
+/// Decoding
+///////////////////////////////////////////////////////////////////////////////////////
+
+   void bmp_stdio_src(bmp_struct_ptr bmp, FILE* file)
    {
       fseek(file, 0, SEEK_SET); // Estableciendo el cursor al inicio
 
@@ -66,7 +88,7 @@ namespace img32::priv
          channel_t b = read8(file);
          channel_t g = read8(file);
          channel_t r = read8(file);
-         address_t dst_address = dstImg->writable_addr32(i, bmp->header.height-scanline-1);
+         address_t dst_address = dstImg->writable_addr32(i, bmp_get_height(bmp)-scanline-1);
          set_pixels_into_address(dst_address, dstImg->colorType(), r, g, b, 255);
       }
 
@@ -81,12 +103,97 @@ namespace img32::priv
       offset(bmp->infile, bmp->array_offset);
 
       for(uint32_t y = 0; y < bmp->header.height; y++) {
-         switch(bmp->header.bitCount) {
+         switch(bmp_get_bitcount(bmp)) {
          case 24:
             read_24bit_bmp(dstImg, y, bmp);
             break;
          default: BMP_ERROR(bmp, "Unsupported bitcount"); break;
          }
+      }
+   }
+   
+///////////////////////////////////////////////////////////////////////////////////////
+/// Encoding
+///////////////////////////////////////////////////////////////////////////////////////
+   void bmp_stdio_dst(bmp_struct_ptr bmp, FILE* file)
+   {
+      bmp->infile = file;
+   }
+
+   void bmp_write_info(bmp_struct_ptr bmp)
+   {
+      using namespace little_endian;
+      FILE* file = bmp->infile;
+      int bpp = 0;
+      int biSizeImage;
+      int bfSize;
+
+      switch(bmp_get_color_type(bmp) & ~EncoderOptions::Alpha_BitMask) {
+      case EncoderOptions::RGB_ColorType:
+         bpp = 24;
+         break;
+      case EncoderOptions::Grayscale_ColorType:
+         bpp = 8;
+      default:
+         BMP_ERROR(bmp, "Unsupported color type!");
+      }
+
+      bmp->header.filler = int((32 - ((bmp_get_width(bmp)*bpp-1) & 31)-1) / 8);
+
+      if(bpp > 8) {
+         biSizeImage = (bmp_get_width(bmp)*3 + bmp->header.filler) * bmp_get_height(bmp);
+         bfSize = BMP_HEADER_SIZE + biSizeImage;
+      }
+
+      write16(file, BMP_FIELD);              // Header field ("BM")
+      write32(file, bfSize);                 // Size BMP file
+      write16(file, NULL);                   // Reserved
+      write16(file, NULL);                   // Reserved
+      if(bpp > 8)
+         write32(file, BMP_HEADER_SIZE);
+      
+      // BMP header
+      write32(file, BMP_INFO_HEADER_SIZE);   // biSize
+      write32(file, bmp_get_width(bmp));     // biWidth
+      write32(file, bmp_get_height(bmp));    // biHeight
+      write16(file, BMP_DEFAULT_PLANES);     // biPlanes
+      write16(file, bpp);                    // biBitCount
+      write32(file, NULL);                   // biCompression
+      write32(file, biSizeImage);            // biSizeImage
+      write32(file, 0xB12);                  // biXPelsPerMeter
+      write32(file, 0xB12);                  // biYPelsPerMeter
+
+      if(bpp > 8) {
+         write32(file, NULL);                // biClrUsed
+         write32(file, NULL);                // biClrImportant
+      }
+   }
+
+   void bmp_write_image(bmp_struct_ptr bmp, const Image& img, const EncoderOptions& opt)
+   {
+      FILE* file = bmp->infile;
+
+      for(int y = bmp_get_height(bmp)-1; y >= 0; y--) {
+         for(int x = 0; x < bmp_get_width(bmp); x++) {
+            color_t c = img.getPixel(x, y);
+
+            if(bmp_get_color_type(bmp) & EncoderOptions::Alpha_BitMask) {
+               if(getA(c) < 255)
+                  c = combine_to_solid_color(c, opt.bmp_background);
+            }else
+               if(getA(c) < 255)
+                  c = opt.bmp_background;
+               
+            if((bmp_get_color_type(bmp) & ~EncoderOptions::Alpha_BitMask) == EncoderOptions::RGB_ColorType) {
+               fputc(getB(c), file);
+               fputc(getG(c), file);
+               fputc(getR(c), file);
+            }else
+               fputc(c, file);
+         }
+
+         for(int i = 0; i < bmp->header.filler; i++)
+            fputc(0, file);
       }
    }
 
@@ -103,23 +210,39 @@ namespace img32::priv
       bmp->error_fn = error_fn;
    }
 
-   uint32_t bmp_get_width(bmp_struct_ptr bmp) {return bmp->header.width;}
-   uint32_t bmp_get_height(bmp_struct_ptr bmp) {return bmp->header.height;}
-
    bool ImageIOPriv::bmp_decode(Image* dstImg, ColorType ct)
    {
-      FileHandle file = open_file(filename(), "rb");
+      FileHandle hFile = open_file(filename(), "rb");
+      FILE* file = hFile.get();
       bmp_struct bmp;
 
       bmp_initialize(&bmp, (bmp_voidp)this, bmp_show_error);
       if(setjmp(bmp._jmp_buf)) return false;
 
-      bmp_start_stdio(&bmp, file.get());
+      bmp_stdio_src(&bmp, file);
       read_header_bmp(&bmp);
       
       dstImg->reset(ImageInfo::Make(bmp_get_width(&bmp), bmp_get_height(&bmp), ct));
       
       read_array_bmp(dstImg, &bmp);
+      return true;
+   }
+
+   bool ImageIOPriv::bmp_encode(const Image& srcImg, const EncoderOptions& options)
+   {
+      FileHandle hFile = open_file(filename(), "wb");
+      FILE* file = hFile.get();
+      bmp_struct bmp;
+
+      bmp_initialize(&bmp, (bmp_voidp)this, bmp_show_error);
+      if(setjmp(bmp._jmp_buf)) return false;
+
+      bmp_stdio_dst(&bmp, file);
+      
+      bmp_set_info(&bmp, srcImg.width(), srcImg.height(), options.colortype);
+      bmp_write_info(&bmp);
+      bmp_write_image(&bmp, srcImg, options);
+
       return true;
    }
 } // namespace img32::priv
